a58dd19b58b6dbd5dacf4bdc3a57ff61b67ad12e
Pito0713 <xli30334@gmail.com>
2021-02-13 19:39:06 +0800

    mernder form

--- src/components/carcart.vue
+++ src/components/carcart.vue
@@ -8,6 +8,8 @@
         <div class="cartText">
           <div>
             <a>{{Product[2]}}</a>
+            <a>共{{Product[4]}}個</a>
+            <a>{{TotalPrice(index)}}</a>
           </div>
           <div class="cartButton">
             <button>
@@ -24,73 +26,74 @@
       </div>
     </div>
     <div class="mender">
-      <table class="mendertable" width="100%">
-        <tr>
-          <td>
-            <a>訂購人資料</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <form action>
-              <tr>
-                <td width=30%>
-                  <a>姓名</a>
-                </td>
-                <td width=100%>   
-                  <input type="text" placeholder="姓名" class="menderinput"/>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label>電話</label>
-                </td>
-                <td>  
-                  <input type="text" placeholder="電話" class="menderinput"/>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label>E-mail</label>
-                  </td>
-                <td> 
-                  <input type="text" placeholder="E-mail" class="menderinput"/>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label>性別</label>
-                  </td>
-                <td> 
-                  <input type="radio" value="男" style="width:20%; font-size:1rem"/>男
-                  <input type="radio" value="女" style="width:20%; font-size:1rem"/>女
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label>地址</label>
-                </td>
-              </tr>
-              <tr>  
-                <td>
-                    <select id="country" name="country">
-                        <option value="australia">Aus</option>
-                        <option value="canada">Canada</option>
-                        <option value="usa">USA</option>
-                    </select> 
-                    縣  
-                    <select id="country" name="country">
-                        <option value="australia">Aus</option>
-                        <option value="canada">Canada</option>
-                        <option value="usa">USA</option>
-                    </select> 
-                    鄉
-                </td>
-              </tr>
-            </form>
-          </td>
-        </tr>
-      </table>
+      <div class="mendertable">
+        <div>
+          <a>訂購人資料</a>
+        </div>
+        <div>
+          <form action>
+            <div class="orderInfo">
+              <label style="flex:40%">姓名</label>
+              <div style="flex:60%">
+                  <input  type="text" placeholder="姓名" class="menderinput" />
+              </div>
+              
+            </div>
+
+            <div class="orderInfo">
+              <label style="flex:40%">行動電話</label>
+              <div style="flex:60%">
+                  <input  type="text" placeholder="電話" class="menderinput" 
+              :class="{ errorborber: isError }"
+              v-model="phoneCheck"
+              />
+              <div v-show="isError" style="color:red">電話號碼有誤</div>
+              </div>
+              
+            </div>
+
+            <div class="orderInfo">
+              <label style="flex:40%">E-mail</label>
+              <div style="flex:60%"> 
+                  <input  type="text" placeholder="E-mail" class="menderinput" />
+              </div>
+              
+            </div>
+            <div class="orderInfo">
+              <label style="flex:40%">性別</label>
+              <div style="flex:60%">
+                <input type="radio" value="男" style="width:20%; font-size:1rem" />男
+                <input type="radio" value="女" style="width:20%; font-size:1rem" />女
+              </div>
+            </div>
+            <div class="orderInfo">
+              <label style="flex:40%">地址</label>
+              <div style="flex:60%">
+                <select id="country" name="country">
+                  <option value="australia">Aus</option>
+                  <option value="canada">Canada</option>
+                </select>
+                縣
+                <select id="country" name="country">
+                  <option value="australia">Aus</option>
+                </select>
+                村
+                <select id="country" name="country">
+                  <option value="australia">Aus</option>
+                </select>
+                鄉
+                <select id="country" name="country">
+                  <option value="australia">Aus</option>
+                </select>
+                街
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div>
+          <button>修改訂單</button>
+      </div>
     </div>
   </div>
 </template>
@@ -100,31 +103,42 @@
 export default {
   data: function() {
     return {
-      Products: [],
-      ProductData: {},
-      Carts: []
+      Carts: [],
+      phoneCheck: '',
+      isError:false,
     };
   },
+  watch:{
+      phoneCheck:function(){
+          let vm =this
+          if(vm.phoneCheck.length < 10){
+            vm.isError = true
+          } else {
+            vm.isError = false;
+          }
+      }
+          
+    },
   methods: {
     filterCart() {
       this.Carts = this.Carts.filter(function(item) {
         return item[4] !== ""; // 篩掉其他
       });
       console.log(this.Carts);
+    },
+    TotalPrice(index) {
+        //let vm = this;
+        console.log(index)
+        //console.log(this.Carts[i][3])
+        //console.log(this.Carts[i][4])
+        var Price = this.Carts[index][3] * this.Carts[index][4];
+        console.log(Price)
+        return Price;
     }
   },
-  computed: {},
+  computed: {
+  },
   mounted() {
-    fetch(
-      "https://script.google.com/macros/s/AKfycbxguddnzylMny9C4bu2lm3ojmd_NYQPw2HjfzmbrPVnV9laIX4/exec"
-    )
-      .then(res => {
-        return res.json();
-      })
-      .then(Products => {
-        this.Products = Products;
-        this.ProductDataStorage = Products[0][4];
-      });
     fetch(
       "https://script.google.com/macros/s/AKfycbxLQARlHh9k7LbV8-ORSmVjIYAJtgphhKXFS0e6ypXmpAWJX8cV/exec"
     )
@@ -134,45 +148,53 @@ export default {
       .then(Carts => {
         this.Carts = Carts;
         this.filterCart();
-        console.log(this.Carts);
+        //console.log(this.Carts);
       });
   }
 };
 </script>
 
 <style  scoped lang="scss">
+button {
+  margin: 1vw;
+  background-color: #ffffff;
+  border: var(--border-color) 1px solid;
+  color: var(--plat-color);
+}
 .orderPage {
   display: flex;
-  height: 40vw;
+  height: auto;
 }
 .cart {
   display: flex;
   flex: 50%;
   flex-direction: column;
   overflow: auto;
+  height: 500px;
 }
 .cartInfo {
   display: flex;
   height: 100px;
   margin: 1rem;
 }
+.orderInfo{
+    display: flex;
+    padding: 0.5rem;
+}
 .mender {
   display: flex;
   flex: 50%;
+  padding: 2vw;
+  flex-direction: column;
 }
 .mendertable {
   border: 1px solid var(--border-color);
+  padding: 2vw;
   form {
-      width: 100%;
-  }
-  td label{
-    font-size: 1.2rem;  
-  }
-  td input{
-      height: 1.5rem
+    width: 100%;
   }
   .menderinput{
-      width: 90%;
+      width: 100%;
   }
 }
 .cartImg {
@@ -192,6 +214,9 @@ export default {
 .cartButton {
   display: flex;
   justify-content: flex-end;
+}
+.errorborber{
+    border:2px solid red;
 }
 @media screen and (max-width: 765px) {
   .orderPage {
